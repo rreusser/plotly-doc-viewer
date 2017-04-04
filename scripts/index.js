bbc7fbb4a81d84ca8f2eaae666ae56573b902ab0
Ricky Reusser <rsreusser@gmail.com>
2017-04-04 02:47:44 -0400

    🐼

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,13 @@ var fs = require('fs');
 var path = require('path');
 var html = require('../lib/index');
 var hyperstream = require('hyperstream');
+var metadataify = require('metadataify');
 var ghcorner = require('github-cornerify');
 
 html({
   script: 'assets/bundle.js',
   content: fs.readFileSync(path.join(__dirname, '../assets/_static.html'), 'utf8')
 })
+  .pipe(metadataify())
   .pipe(ghcorner())
   .pipe(process.stdout);
